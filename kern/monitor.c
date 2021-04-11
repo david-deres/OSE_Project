d2a941ac0e60b55fbb18e52e326d82b91ba3cfa3
Niv Kaminer <nivkner@zoho.com>
2021-04-11 17:29:38 +0300

    Revert "convert show to manipulate pages instead of addresses"
    the instructions explicitly ask for the display of mapping in a specific address range,
    not by pages
    
    This reverts commit c458cbe677ff0987c1a34abe2a85fa451bcfb1fc.

--- kern/monitor.c
+++ kern/monitor.c
@@ -35,7 +35,7 @@ perm {vstart} {vend} [R/RW/RU/RWU] - set the permissions\n\
     of the virtual pages, where R is read-only, RW is for read-write\n\
     and U can be added to any of them to indicate user access\n\
 show {vstart} {vend} - show the mapping and permissions\n\
-    for virtual pages vstart-vend\n\
+    for pages contating virtual addresses vstart-vend\n\
 dump [v/p] {start} {end}- dump the contents of the addresses start-end\n\
     those are interpreted as virtual with 'v' or physical with 'p'",
 mon_vmmap },
