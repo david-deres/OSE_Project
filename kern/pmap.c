0c89ae17c766171b08760f9716459f3271e3119a
Niv Kaminer <nivkner@zoho.com>
2021-04-21 13:38:49 +0300

    implement user_mem_check

--- kern/pmap.c
+++ kern/pmap.c
@@ -568,6 +568,19 @@ int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
 	// LAB 3: Your code here.
+    uintptr_t page_addr = ROUNDDOWN((uintptr_t)va, PGSIZE);
+    uintptr_t range_end = ROUNDUP((uintptr_t)va + len, PGSIZE);
+    for (;page_addr < range_end; page_addr += PGSIZE) {
+        if (page_addr >= ULIM) {
+            user_mem_check_addr = page_addr;
+            return -E_FAULT;
+        }
+        pte_t *pte = pgdir_walk(env->env_pgdir, (void*)page_addr, false);
+        if (pte == NULL || (*pte & (perm | PTE_P)) == 0) {
+            user_mem_check_addr = page_addr;
+            return -E_FAULT;
+        }
+    }
 
 	return 0;
 }
