49cfe855738605899baa4745200906227bd987e6
Niv Kaminer <nivkner@zoho.com>
2021-04-21 09:55:07 +0300

    implement a syscall handler stub

--- kern/trap.c
+++ kern/trap.c
@@ -162,6 +162,16 @@ trap_dispatch(struct Trapframe *tf)
             break;
         case T_BRKPT:
             monitor(tf);
+            break;
+        case T_SYSCALL:
+            tf->tf_regs.reg_eax =
+                syscall(tf->tf_regs.reg_eax,
+                        tf->tf_regs.reg_edx,
+                        tf->tf_regs.reg_ecx,
+                        tf->tf_regs.reg_ebx,
+                        tf->tf_regs.reg_edi,
+                        tf->tf_regs.reg_esi);
+            break;
         default:
             ; // fallthrough on unexpected trap
     }
