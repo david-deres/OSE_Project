24e6276d24cf86a34e10c4e3669183c75b17d573
Niv Kaminer <nivkner@zoho.com>
2021-05-14 20:01:55 +0300

    implement exercise 1

--- kern/env.c
+++ kern/env.c
@@ -423,6 +423,9 @@ env_create(uint8_t *binary, enum EnvType type)
 
 	// If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
 	// LAB 5: Your code here.
+    if (type == ENV_TYPE_FS) {
+        newEnv->env_tf.tf_eflags |= FL_IOPL_3;
+    }
 }
 
 //
