e90c8b7423b0524de58f2e8b56209a1644fb11ec
Niv Kaminer <nivkner@zoho.com>
2021-05-17 19:51:31 +0300

    truncate write request instead of asserting

--- lib/file.c
+++ lib/file.c
@@ -142,8 +142,8 @@ devfile_write(struct Fd *fd, const void *buf, size_t n)
 	// bytes than requested.
 	// LAB 5: Your code here
 	int r;
-	// check that the size is at most the req_buf size
-	assert(n <= PGSIZE - (sizeof(int) + sizeof(size_t)));
+	// ensure that the size is at most the req_buf size
+    n = MIN(n, PGSIZE - (sizeof(int) + sizeof(size_t)));
 	fsipcbuf.write.req_fileid = fd->fd_file.id;
 	fsipcbuf.write.req_n = n;
 	memmove(fsipcbuf.write.req_buf, buf, n);
