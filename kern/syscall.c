9259fe4c3d1284719b87d5a7d0c9cb3a546510a9
David Deres <david.deres@cs.technion.ac.il>
2021-06-08 17:50:36 +0300

    added syscall for receive

--- kern/syscall.c
+++ kern/syscall.c
@@ -489,6 +489,24 @@ int32_t sys_net_try_send(void *va, size_t length) {
     return r;
 }
 
+// receive a packet from the network.
+// sleeps until there is one to receive.
+// Return 0 on success, < 0 on error.  Errors are:
+//     -E_INVAL if the env doesn't have permission to read the memory,
+//              or the va isn't page aligned
+int32_t sys_net_recv(void *va, size_t *pkt_size) {
+    if (user_mem_check(curenv, va, PGSIZE, PTE_P | PTE_U) != 0) {
+        return -E_INVAL;
+    }
+    uintptr_t page_start = ROUNDDOWN((uintptr_t)va, PGSIZE);
+    // check if address is page aligned, maybe this is unnecessary?
+    if (page_start != (uintptr_t)va){
+        return -E_INVAL;
+    }
+    int r = receive_packet(va, pkt_size);
+    return r;
+}
+
 // Dispatches to the correct kernel function, passing the arguments.
 int32_t
 syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, uint32_t a5)
@@ -532,6 +550,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
             return sys_time_msec();
         case SYS_net_try_send:
             return sys_net_try_send((void*)a1, a2);
+        case SYS_net_recv:
+            return sys_net_recv((void*)a1, (size_t*)a2);    
         default:
             return -E_INVAL;
 	}
--- lib/syscall.c
+++ lib/syscall.c
@@ -126,3 +126,7 @@ sys_time_msec(void)
 int sys_net_try_send(void *va, size_t length) {
     return syscall(SYS_net_try_send, true, (uint32_t)va, length, 0, 0, 0);
 }
+
+int sys_net_recv(void *va, uint32_t pkt_size) {
+    return syscall(SYS_net_recv, true, (uint32_t)va, pkt_size, 0, 0, 0);
+}
