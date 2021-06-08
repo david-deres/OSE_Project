c652b50d9299cfeb9e4029c0e9ac14d20d3b74f5
David Deres <david.deres@cs.technion.ac.il>
2021-06-08 20:54:36 +0300

    implemented input.c - not stable yet

--- net/input.c
+++ net/input.c
@@ -1,6 +1,12 @@
 #include "ns.h"
+#include <inc/lib.h>
 
 extern union Nsipc nsipcbuf;
+
+#define BUFFER_SIZE 2048
+
+
+
 
 void
 input(envid_t ns_envid)
@@ -13,4 +19,27 @@ input(envid_t ns_envid)
 	// Hint: When you IPC a page to the network server, it will be
 	// reading from it for a while, so don't immediately receive
 	// another packet in to the same physical page.
+
+	int r, i;
+	uint8_t input_buffer[BUFFER_SIZE];
+
+	while(1){
+		size_t* len = 0;
+		void* buff = &nsipcbuf.pkt.jp_data;
+		void *va = &nsipcbuf.pkt.jp_len;
+		memset(input_buffer, 0, BUFFER_SIZE);
+		if ((r = sys_net_recv(input_buffer, len))<0){
+			panic("input error: %e\n", r);
+		}
+		if (len<0){
+			panic("input error: received invalid length\n");
+		}
+		memcpy(buff, input_buffer, *len);
+		nsipcbuf.pkt.jp_len = *len;
+		ipc_send(ns_envid, NSREQ_INPUT, va, PTE_P | PTE_U);
+		//not sure why
+		//sys_page_unmap(curenv->env_id, &nsipcbuf.pkt);
+		//wait for the page to be copied, maybe do this for #CPU's 
+		sys_yield();
+	}
 }
