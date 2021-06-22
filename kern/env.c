c1053c10a87607ffb4a8091ff2a09611e91fa9ca
David Deres <david.deres@cs.technion.ac.il>
2021-06-22 19:30:39 +0300

    init env_ipc_multi_recv to 0

--- kern/env.c
+++ kern/env.c
@@ -267,6 +267,7 @@ env_alloc(struct Env **newenv_store, envid_t parent_id)
 
 	// Also clear the IPC receiving flag.
 	e->env_ipc_recving = 0;
+	e->env_ipc_multi_recving = 0;
 
 	// clear the IO wait flag
 	e->env_waits_for_input = false;
