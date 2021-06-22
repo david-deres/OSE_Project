3ba22f198093455a833c8c729af420405cc9388f
David Deres <david.deres@cs.technion.ac.il>
2021-06-22 21:43:42 +0300

    output works with nsipcbuf array instead of pgvec

--- net/output.c
+++ net/output.c
@@ -1,7 +1,7 @@
 #include "ns.h"
 #include <inc/lib.h>
 
-extern union Nsipc nsipcbuf;
+extern union Nsipc nsipcbuf[16];
 
 // block the thread untill the packet has been sent to the driver
 static int send_packet(void *buffer, size_t length, bool isEOP) {
@@ -27,7 +27,7 @@ output(envid_t ns_envid)
 
     while (true) {
         int perm = 0;
-        void *va = &nsipcbuf.pkt.jp_len;
+        void *va = &nsipcbuf[0].pkt.jp_len;
         envid_t whom = 0;
         int req_type = ipc_recv(&whom, va, &perm);
         if (req_type < 0) {
@@ -38,15 +38,15 @@ output(envid_t ns_envid)
             if (!(perm & PTE_P)) {
 			    panic("buffer missing from message to output env\n");
 		    }
-            send_packet(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len, true);
-            sys_page_unmap(curenv->env_id, &nsipcbuf.pkt);
+            send_packet(nsipcbuf[0].pkt.jp_data, nsipcbuf[0].pkt.jp_len, true);
+            sys_page_unmap(curenv->env_id, &nsipcbuf[0].pkt);
         }
         else if (req_type == NSREQ_OUTPUT_MULTI) {
             int j;
             for (j = 0; j < 16; j++){
                 lenghts[j] = 0;
             }
-            int num_of_pkts = ipc_recv_multi(&buffers, &perm);
+            int num_of_pkts = ipc_recv_multi(&nsipcbuf, &perm);
             if (num_of_pkts <= 0){
                     panic("receiving multiple IPC on output env failed: %e\n", num_of_pkts);
             }
@@ -55,11 +55,14 @@ output(envid_t ns_envid)
             }  
             int i;
             for (i=0; i < num_of_pkts - 1; i++){
-                send_packet(buffers.pgv_base[i], lenghts[i], false);
+                //send_packet(buffers.pgv_base[i], lenghts[i], false);
+                send_packet(nsipcbuf[i].pkt.jp_data, nsipcbuf[i].pkt.jp_len, false);
             }
-            send_packet(buffers.pgv_base[i],lenghts[i], true);
+            //send_packet(buffers.pgv_base[i],lenghts[i], true);
+            send_packet(nsipcbuf[i].pkt.jp_data, nsipcbuf[i].pkt.jp_len, true);
             for (i=0; i < num_of_pkts; i++){
-                sys_page_unmap(curenv->env_id, (void *)ROUNDDOWN(buffers.pgv_base[i], PGSIZE));
+                //sys_page_unmap(curenv->env_id, (void *)ROUNDDOWN(buffers.pgv_base[i], PGSIZE));
+                sys_page_unmap(curenv->env_id, (void *)nsipcbuf[i].pkt.jp_data);
             }
         }
         else{
